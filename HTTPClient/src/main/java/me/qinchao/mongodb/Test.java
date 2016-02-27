25fa1fcf7b7f7e78b260ab5a6a7a359721a648fb
sulvto <sulvto@gmail.com>
2016-02-27 23:01:45 +0800

    add spring-data-mongodb

--- HTTPClient/src/main/java/me/qinchao/mongodb/Test.java
+++ HTTPClient/src/main/java/me/qinchao/mongodb/Test.java
@@ -1,37 +1,51 @@
 package me.qinchao.mongodb;
 
+import com.alibaba.fastjson.JSON;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
+import com.mongodb.Mongo;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+
+import java.util.Set;
 
 public class Test {
 	public static void main(String[] args) {
 
-		DBCollection collection = MongoManager.getCollection("qinchao", "test");
-		
-		
-		final BasicDBObject basicDBObject = new BasicDBObject("name", "qinchao");
-		basicDBObject.append("age", 19);
-//		collection.insert(basicDBObject);
+		MongoOperations mongoOps = new MongoTemplate(new SimpleMongoDbFactory(new Mongo(), "qinchao"));
+		Set<String> collectionNames = mongoOps.getCollectionNames();
+		System.out.println(JSON.toJSONString(collectionNames));
 
-		final DBCursor cursor = collection.find();
-		while (cursor.hasNext()) {
-			System.out.println(cursor.next());
-		}
-		cursor.close();
-		
-		
-		
-
-		MongoCollection<?> mongoCollection = MongoManager.getMongoCollection("qinchao", "test");
-		FindIterable<?> find = mongoCollection.find();
-		final MongoCursor<?> iterator = find.iterator();
-		while (iterator.hasNext()) {
-			System.out.println(iterator.next());			
-		}
+//
+//		DBCollection collection = MongoManager.getCollection("qinchao", "test");
+//
+//
+//
+//		final BasicDBObject basicDBObject = new BasicDBObject("name", "qinchao");
+//		basicDBObject.append("age", 19);
+////		collection.insert(basicDBObject);
+//
+//		final DBCursor cursor = collection.find();
+//		while (cursor.hasNext()) {
+//			System.out.println(cursor.next());
+//		}
+//		cursor.close();
+//
+//
+//
+//
+//		MongoCollection<?> mongoCollection = MongoManager.getMongoCollection("qinchao", "test");
+//		FindIterable<?> find = mongoCollection.find();
+//		final MongoCursor<?> iterator = find.iterator();
+//		while (iterator.hasNext()) {
+//			System.out.println(iterator.next());
+//		}
 
 
 
